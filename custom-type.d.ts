72f38ce0fe0a746706d206070e0ca41c97df835c
Isman Usoh <isman.isman@bitkub.com>
2021-05-28 01:29:35 +0700

    fix: typescript custom type

--- custom-type.d.ts
+++ custom-type.d.ts
@@ -1,6 +1,5 @@
 
-declare module "redux-wait-for-action" {
-    export const WAIT_FOR_ACTION, ERROR_ACTION, CALLBACK_ARGUMENT, CALLBACK_ERROR_ARGUMENT
-    const middleware = () => any
-    export default middleware
+declare module 'redux-wait-for-action' {
+    const module: any
+    export = module
 }
